edcfab0500a421e4e4246504d57654c7206bc352
AmolchimadagE <131466503+AmolchimadagE@users.noreply.github.com>
2024-07-30 13:31:33 +0530

    first commit

---
+++ src/components/Attribution.js
@@ -0,0 +1,7 @@
+export default function Attribution() {
+   return (
+      <footer className="attribution">
+         <p>developed by <a href="https://amolportfolio1.vercel.app/" target="_blank" rel="noreferrer">Amol</a></p>
+      </footer>
+   );
+}
